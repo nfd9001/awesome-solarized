6dddea50850910af79e28359e7581272ad6a5f1d
nfd9001 <alexander@ticky-box.com>
2013-10-11 11:01:08 -0700

    Added wallpaper (based off Zenburn wallpaper, tweaked to use Solarized Dark colors)

--- dark/theme.lua
+++ dark/theme.lua
@@ -9,7 +9,7 @@
 -- {{{ Main
 theme = {}
 theme.default_themes_path = "/usr/share/awesome/themes"
-theme.wallpaper_cmd = { "awsetbg "..theme.default_themes_path.."/zenburn/zenburn-background.png" }
+theme.wallpaper_cmd = {"awsetbg "..theme.default_themes_path.."/awesome-solarized/dark/zenburnSolarizedWallpaper.png"  }
 theme.colors = {}
 theme.colors.base3   = "#002b36ff"
 theme.colors.base2   = "#073642ff"
